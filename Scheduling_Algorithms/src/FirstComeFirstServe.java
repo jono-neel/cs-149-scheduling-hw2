a784934ad9756e2fbcef3628bd14ca59050a9d90
Katherine Soohoo <katherinesoohoo@gmail.com>
2016-02-21 18:52:36 -0800

    fixed algorithm, added time updates

--- Scheduling_Algorithms/src/FirstComeFirstServe.java
+++ Scheduling_Algorithms/src/FirstComeFirstServe.java
@@ -7,40 +7,60 @@ import java.util.ArrayDeque;
  * @author Jonathan Neel
  *
  */
-public class FirstComeFirstServe extends SchedulingAlgorithm{
-	public FirstComeFirstServe(ArrayDeque<ProcessSim> list)
-	{
-            super(list);
-	}
-	@Override
-	public void run() {
-		ArrayDeque<ProcessSim> processQueue = new ArrayDeque<ProcessSim>();
-		int throughput = 0;
-		while(quantum < 100) //stop after 99 quantum
-		{
-			
-			while(!processList.isEmpty() && quantum > processList.peek().getArrivalTime()) 
-				//when the scheduler reaches the next arrival time of the processes
-			{
-				processQueue.add(processList.pop()); //put the next process into the queue
-			}
-			if(processQueue.isEmpty()) //idle if there are no more processes entering the queue
-			{
-				quantum += .1;
-			}
-			else
-			{
-				while(!processQueue.isEmpty()) //run the processes that have reached the cpu
-				{
-					ProcessSim currentProcess = processQueue.remove();
-					quantum += currentProcess.getRunTime();
-					throughput++;
-					System.out.println("Quantum: " + (quantum - currentProcess.getRunTime()) + " Process: " + currentProcess.getName());
-					if(quantum > 99) break;
-				}
-			}
-		}System.out.println("Throughput:" + throughput);
-		
-	}
+public class FirstComeFirstServe extends SchedulingAlgorithm
+{
+    public FirstComeFirstServe(ArrayDeque<ProcessSim> list)
+    {
+        super(list);
+    }
+    
+    @Override
+    public void run()
+    {
+        ArrayDeque<ProcessSim> processQueue = new ArrayDeque<ProcessSim>();
+        ProcessSim currentProcess;
+        while(quantum < 100) //stop after 99 quantum
+        {			
+            while(!this.processList.isEmpty() && quantum > this.processList.peek().getArrivalTime()) 
+                //when the scheduler reaches the next arrival time of the processes
+            {
+                processQueue.add(this.processList.pop()); //put the next process into the queue
+            }
 
+            if(!processQueue.isEmpty()) //run the processes that have reached the cpu
+            {
+                currentProcess = processQueue.remove();
+                updateTimes(currentProcess);
+                while(currentProcess.getRemainingRunTime() > 0)
+                {
+                    currentProcess.setRemainingRunTime(currentProcess.getRemainingRunTime() - 1);
+                    quantum += 1;
+                    timeChart.add(currentProcess);
+                }
+                //quantum += currentProcess.getRunTime();
+                //throughput++;
+                //System.out.println("Quantum: " + (quantum - currentProcess.getRunTime()) + " Process: " + currentProcess.getName());
+                //if(quantum > 99) break;
+            }
+            else
+            {
+                quantum += 1;
+                timeChart.add(new ProcessSim());
+            }
+
+        }
+        
+        // execute remaining processes
+        while(!processQueue.isEmpty())
+        {
+            currentProcess = processQueue.remove();
+            updateTimes(currentProcess);
+            while(currentProcess.getRemainingRunTime() > 0)
+            {
+                currentProcess.setRemainingRunTime(currentProcess.getRemainingRunTime() - 1);
+                quantum += 1;
+                timeChart.add(currentProcess);
+            }
+        }
+    }
 }
