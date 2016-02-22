ddd88add7799826d9b6c05fca8dd693c6b7a2db8
Katherine Soohoo <katherinesoohoo@gmail.com>
2016-02-21 16:22:08 -0800

    changed to abstract class, includes timing fields and statistical print method

--- Scheduling_Algorithms/src/SchedulingAlgorithm.java
+++ Scheduling_Algorithms/src/SchedulingAlgorithm.java
@@ -1,15 +1,52 @@
-import java.util.ArrayList;
+
+import java.util.ArrayDeque;
 
 /**
- * An interface that all scheduling algorithms should follow. May need to add more
+ * An abstract class that all scheduling algorithms should inherit. May need to add more
  * or may not be needed at all, I'm not sure.
- * @author Jonathan Neel
+ * @author Jonathan Neel, Katherine Soohoo
  *
  */
-public interface SchedulingAlgorithm {
-	/**
-	 * Runs the algorithm, given a list of processes in order of arrival time.
-	 * @param list the list of processes to be run
-	 */
-	public void run(ArrayList<ProcessSim> list);
+public abstract class SchedulingAlgorithm
+{
+    protected float quantum = 0;
+    protected float totalTurnaroundTime = 0;
+    protected float totalWaitTime = 0;
+    protected float totalResponseTime = 0;
+    protected ArrayDeque<ProcessSim> processList;
+    protected int processListSize;
+    
+    /**
+     * Creates a scheduling algorithm with list of simulated processes.
+     * @param list list of simulated processes
+     */
+    public SchedulingAlgorithm(ArrayDeque<ProcessSim> list)
+    {
+        processList = list;
+        processListSize = list.size();
+    }
+    
+    /**
+     * Runs scheduler.
+     */
+    public abstract void run();
+    
+    /**
+     * Prints algorithm run statistics
+     * Turnaround Time = wait time + run time
+     * Wait Time = arrival time + time when execution begins
+     * Response Time = ?
+     * Throughput = number of jobs / total time
+     */
+    protected void printRun()
+    {
+        System.out.println("Average Turnaround Time: "
+                + totalTurnaroundTime / processListSize);
+        System.out.println("Average Wait Time: "
+                + totalWaitTime / processListSize);
+        System.out.println("Average Response Time: "
+                + totalResponseTime / processListSize);
+        System.out.println("Throuhput: "
+                + quantum / processListSize);
+    }
 }
