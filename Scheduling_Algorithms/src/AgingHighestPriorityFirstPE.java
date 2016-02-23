ec5bc33d5f3035d720565cda394b4c3f433fbba1
tincheleky <bebindenhat@yahoo.com>
2016-02-23 01:02:15 -0800

    Fixed bugs

--- Scheduling_Algorithms/src/AgingHighestPriorityFirstPE.java
+++ Scheduling_Algorithms/src/AgingHighestPriorityFirstPE.java
@@ -179,16 +179,20 @@ public class AgingHighestPriorityFirstPE extends SchedulingAlgorithm {
         //System.out.println("START INC PRI");
         //Increase priority if a process waited for 5 quantum.
         ArrayList<ProcessSim> temp = new ArrayList<>();
+        ProcessSim ps;
         for(int i = 0; i < MAX_PRIORITY; i++){
             PriorityQueue<ProcessSim> tem = queueList.get(i + 1);
-            for(ProcessSim ps : temp)
+            int size = tem.size();
+            while(size-- > 0)
             {
+                ps = tem.poll();
                 if(ps.getWaitedQuantum() >= 5){
                     ps.increasePriority();
-                    queueList.get(i + 1).remove(ps);
-                    queueList.get(i).add(ps);
                 }
+                temp.add(ps);
             }
+            for(ProcessSim pss: temp)
+                tem.add(pss);
         }        
     }
     
@@ -196,11 +200,18 @@ public class AgingHighestPriorityFirstPE extends SchedulingAlgorithm {
     {
         //System.out.println("START INC WAITEDQUANTUM");
         ArrayList<ProcessSim> temp = new ArrayList<>();
+        ProcessSim ps;
         for(int i = 0; i < MAX_PRIORITY; i++){
             PriorityQueue<ProcessSim> tem = queueList.get(i + 1);
-            for(ProcessSim ps : temp){
+            int size = tem.size();
+            while(size-- > 0)
+            {
+                ps = tem.poll();
                 ps.increaseWaitedQuantum();
+                temp.add(ps);
             }
+            for(ProcessSim pss: temp)
+                tem.add(pss);
         } 
     }
 }
