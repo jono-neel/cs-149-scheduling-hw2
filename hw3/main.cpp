bb3bbebe34d3ad677b5d69e3fd9d6e3f8cfe86c9
tincheleky <bebindenhat@yahoo.com>
2016-02-26 09:49:25 -0800

    Less headache : )

--- hw3/main.cpp
+++ hw3/main.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <ctime>
 #include <stdlib.h>
-#include <queue>
+#include <vector>
 #include <pthread.h>
 
 #define CHART_SIZE 10
@@ -15,42 +15,22 @@ using namespace std;
 enum SellerType { H = 72, M = 77, L = 76 };
 
 class Seller;
-//class Customer;
 class Seat;
 
 class Customer
 {
-    public:
+    private:
         string name;
         Seller *seller;
 
     public:
-        Customer()
-        {
-            name = "";
-            seller = NULL;
-        }
-        Customer(Seller *s)
-        {
-            name = "";
-            seller = s;
-        }
-        string getName()
-        {
-            if(name == "")
-                return "N/A";
-            else
-                return name;
-        }
-
-        string getSellerInfo()
-        {
-            if(!seller)
-              return "N/A";
-            else
-               return "";
-        }
+        Customer();
+        Customer(Seller *s);
+        
+        string getName();
+        string getSellerInfo();
 };
+
 
 class Seat
 {
@@ -59,21 +39,13 @@ class Seat
         Customer *customer;
 
     public:
-        Seat()
-        {
-            isAvailable = false;
-            customer = NULL;
-        };
+        Seat();
 
-        void setAvailable(){};
-        void getAvailable(){};
-        void print()
-        {
-            if (!customer)
-                cout << "---";
-            else
-                cout << customer->getSellerInfo();
-        }
+        void setAvailable();
+        bool getAvailable();
+        void setCustomer(Customer* pCus);
+        Customer* getCustomer();
+        void print();
 };
 
 class Seller
@@ -81,36 +53,106 @@ class Seller
     private:
         string name;
         SellerType type;
-        queue<Customer> customerQueue;
+        vector<Customer> customerQueue;
 
     public:
-        Seller()
-        {
-            name = "";
-        }
-        Seller(int n, SellerType t)
-        {
-            name = (char)t;
-            name += (char)(n + 48);
-            type = t;
-        }
+        Seller();
+        Seller(int n, SellerType t);
 
-        int generateWaitTime()
-        {
-            switch(type)
-            {
-                case H : return (rand() % (MAX_H_RANGE - MIN_H_RANGE + 1) + MIN_H_RANGE);
-                //(rand() % (max - min + 1) + min) should give a value in range of max - min
-                case M : return (rand() % (MAX_M_RANGE - MAX_H_RANGE + 1) + MAX_H_RANGE);
-                case L : return (rand() % (MAX_L_RANGE - MAX_M_RANGE + 1) + MAX_M_RANGE);
-            };
-        };
-
-        string getInfo()
-        {
-            return name;
-        }
+        int generateWaitTime();
+        string getInfo();
 };
+
+Customer::Customer()
+{
+    name = "";
+    seller = NULL;
+}
+
+Customer::Customer(Seller *s)
+{
+    name = "";
+    seller = s;
+}
+
+string Customer::getName()
+{
+    if(name == "")
+        return "N/A";
+    else
+        return name;
+}
+
+string Customer::getSellerInfo()
+{
+    if(!seller)
+      return "N/A";
+    else
+       return seller->getInfo();
+}
+
+Seat::Seat()
+{
+    isAvailable = false;
+    customer = NULL;
+};
+
+void Seat::setAvailable()
+{
+    isAvailable = false;
+};
+
+bool Seat::getAvailable(){
+    return isAvailable;
+};
+
+void Seat::setCustomer(Customer* pCus)
+{
+    customer = pCus;
+}
+
+Customer* Seat::getCustomer()
+{
+    return customer;
+}
+
+void Seat::print()
+{
+    if (!customer)
+        cout << "---";
+    else
+        cout << customer->getSellerInfo();
+}
+
+
+Seller::Seller()
+{
+    name = "";
+}
+
+Seller::Seller(int n, SellerType t)
+{
+    name = (char)t;
+    name += (char)(n + 48);
+    type = t;
+}
+
+int Seller::generateWaitTime()
+{
+    switch(type)
+    {
+        //(rand() % (max - min + 1) + min) should give a value in range of max - min
+        case H : return (rand() % (MAX_H_RANGE - MIN_H_RANGE + 1) + MIN_H_RANGE);
+        case M : return (rand() % (MAX_M_RANGE - MAX_H_RANGE + 1) + MAX_H_RANGE);
+        case L : return (rand() % (MAX_L_RANGE - MAX_M_RANGE + 1) + MAX_M_RANGE);
+    };
+};
+
+string Seller::getInfo()
+{
+    return name;
+}
+
 
 // main() is where program execution begins.
 int main()
@@ -120,15 +162,7 @@ int main()
     Seller s2(1, L);
     Seller s3(1, M);
 
-
     pthread_t thread1, thread2;
-
-    Customer cusArr[10];
-    cusArr[0].name = "A";
-    cusArr[1].name = "B";
-    cusArr[2].name = "C";
-    cusArr[3].name = "D";
-    cusArr[4].name = "E";
 
     // set the seed
     srand((unsigned)time(NULL));
