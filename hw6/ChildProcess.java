6473a26e0fde93baf9caec63f7a3f544b93a7e91
Katherine Soohoo <katherinesoohoo@gmail.com>
2016-04-14 00:56:32 -0700

    moved and renamed some variables, edited output

--- hw6/ChildProcess.java
+++ hw6/ChildProcess.java
@@ -10,71 +10,74 @@ import java.util.logging.Logger;
 
 /**
  * Child processes is connected to parent process via a single pipe.
- * Child processes write messages with time-stamps to their pipe.
- * 
+ * Child processes write messages with timeStamp-stamps to their pipe.
+ *
  */
 public class ChildProcess implements Runnable
 {
-	long startTime, currentTime;
-	final long END_TIME = 30000; //stop after 30 seconds
+	static final long END_TIME = 30000; //stop after 30 seconds
+	long startTime;
 
-	Pipe pipe; 
-
+	Pipe pipe;
 	ByteBuffer buf;
+
 	int childName;
-	int messageNumber;
+
 	/**
 	 * Each child process has a pipe which it uses to send messages to the parent process.
 	 * Behavior of message depends on the child process (child 5 waits for user input)
 	 * @param name the number of the process
-	 * @param startTime the start time
+	 * @param startTime the start timeStamp
 	 * @param pipe the pipe corresponding to this child process
 	 */
 	public ChildProcess(int name, long startTime, Pipe pipe)
-	{ 
+	{
 		this.startTime = startTime;
 		this.pipe = pipe;
-		messageNumber = 0;
-		childName = name;
 		buf = ByteBuffer.allocate(128);
+		childName = name;
 	}
 
 	@Override
-	public void run() 
-	{	
+	public void run()
+	{
 		String message;
+		int messageNumber = 0;
+		double curTime;
+		String timeStamp;
+
 		while(System.currentTimeMillis() - startTime < END_TIME) //stop all processes after 30 secs
 		{
-			double t = (double) ((System.currentTimeMillis() - startTime) / 1000.0);
-			String time = String.format("%06.3f", t); //the elapsed time
 			if(childName == 4) //5th child waits for input from user
 			{
-				System.out.println("Enter a message: ");
+				System.out.print("Enter a message: ");
 				Scanner in = new Scanner(System.in);
+				// format message with timestamp
+				curTime = (double) ((System.currentTimeMillis() - startTime) / 1000.0);
+				timeStamp = String.format("%06.3f", curTime);
 				messageNumber++;
-				message = "User message: ";
-				message += in.nextLine();
-				t = (double) ((System.currentTimeMillis() - startTime) / 1000.0);
-				time = String.format("%06.3f", t); //child time stamp
-				message += " @ 0:" + time + ": Child " + (childName + 1) + 
-						": UserMessage " + messageNumber + "\n";
+				message =  "0:" + timeStamp + ": Child " + (childName + 1) +
+						" " + in.nextLine() + "\n";
 			}
-			else
+			else // other children generate automated message
 			{
+				//sleep for random time between 0 - 2 seconds
 				Random rando = new Random();
-				long restTime = rando.nextInt(3) * 1000; 
+				long restTime = rando.nextInt(3) * 1000;
 				try {
-					Thread.sleep(restTime); //sleep for random time between 0 - 2 seconds
+					Thread.sleep(restTime);
 				} catch (InterruptedException e2) {
 					e2.printStackTrace();
 				}
-
+				// format message with timestamp
+				curTime = (double) ((System.currentTimeMillis() - startTime) / 1000.0);
+				timeStamp = String.format("%06.3f", curTime);
 				messageNumber++;
-				message = "0:" + time + ": Child " + (childName + 1) +
-						" : Message " + messageNumber + "\n";
+				message = "0:" + timeStamp + ": Child " + (childName + 1) +
+						" message " + messageNumber + "\n";
 
-			}	
-			try {
+			}
+			try { // write message to pipe
 				buf.clear();
 				buf.put(message.getBytes()); //put message in buffer
 				buf.flip();
@@ -88,4 +91,3 @@ public class ChildProcess implements Runnable
 		}
 	}
 }
-
